6795422572de43381558d5639477236d93274249
Urquhart <peyton.urquhart@wsu.edu>
2021-04-03 11:10:18 -0700

    Common-Movies

--- functions/functions-imdb.R
+++ functions/functions-imdb.R
@@ -1,5 +1,53 @@
 
-test77 = function()
+# Return a df of movies which exist in both dataframes
+findCommonMovies = function(mdf1, mdf2)
 {
-  print("testing");
+  n = 0;
+  id1 = 0;
+  for(i in 1:nrow(mdf1)) 
+  {
+    ttid1 <- mdf1[i,1];
+    for(k in 1:nrow(mdf2)) 
+    {
+      ttid2 <- mdf2[k,1];
+      
+      if(ttid2 == ttid1)
+      {
+        if(n == 0)
+        {
+          id1 = k;
+        }
+        
+        n = n + 1;
+      }
+    }
+    
+  }
+  if (n == 0)
+  {
+    print("Zero common movies found");
+    return(NULL);
+  }
+  ndf <- mdf2[c(id1),]
+  if (n == 1)
+  {
+    print("1 common movie found");
+    return(ndf);
+  }
+  for(i in 1:nrow(mdf1)) 
+  {
+    ttid1 <- mdf1[i,1];
+    for(k in 1:nrow(mdf2)) 
+    {
+      ttid2 <- mdf2[k,1];
+      
+      if(ttid2 == ttid1 && k != id1)
+      {
+        ndf <- rbind(ndf, mdf2[c(k), ])
+      }
+    }
+  }
+  
+  print("Multiple common movies found");
+  return(ndf)
 }
